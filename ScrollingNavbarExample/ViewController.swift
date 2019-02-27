e6d0937ac987caed112384269843bfb679436041
Sachin <sachin@wizely.in>
2019-02-27 13:34:16 +0530

    Initial Commit

---
+++ ScrollingNavbarExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScrollingNavbarExample
+//
+//  Created by Sachin Nautiyal on 2/27/19.
+//  Copyright © 2019 Sachin Nautiyal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
